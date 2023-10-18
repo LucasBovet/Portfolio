377ccbb2823308beadfaacc80989152026678727
LucasBovet <74911542+LucasBovet@users.noreply.github.com>
2023-10-18 08:05:38 +0200

    Delete PORFOLIO LUCAS/PORFOLIO LUCAS directory

--- PORFOLIO LUCAS/PORFOLIO LUCAS/js/index.js
+++
@@ -1,9 +0,0 @@
-function openMenu(){
-    const nav = document.getElementById('nav');
-
-    if(nav.style.display === 'block'){
-        nav.style.display = 'none';
-    }else{
-        nav.style.display = 'block';
-    }
-}
